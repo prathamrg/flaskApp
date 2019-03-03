9d11b7201b9fd551dacfb965aa22dedd6e7c6f52
prathamrg <prathamgoel95@gmail.com>
2019-03-03 13:18:17 +0530

    Create helper.py

---
+++ utils/helper.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 13:05:30 2019
+
+@author: prajkumargoel
+"""
+
+class Response():
+    
+    def makeResponse(text):
+        return {
+                "fulfillmentText"     : text,
+                "fulfillmentMessages" : [text]
+            }
