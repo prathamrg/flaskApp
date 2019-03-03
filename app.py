1287724328b6de954fc06527e5ed327105716c8f
prathamrg <prathamgoel95@gmail.com>
2019-03-03 11:35:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def get_tasks3():
     input = req.get("queryResult").get("queryText")
     output = {
                 "fulfillmentText"     : "Hello There. API Call Successful using {} webhook".format(input),
-                "fulfillmentMessages" : [{"text": {"text":["Hello There. API Call Successful"]}}]
+                "fulfillmentMessages" : [{"text": {"text":["Hello There. API Call Successful using {} webhook".format(input)]}}]
             
             
             }
