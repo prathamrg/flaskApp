a09e440c31a689652924ecfc8f7da6a4010fd8df
prathamrg <prathamgoel95@gmail.com>
2019-03-06 16:19:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,12 +45,10 @@ def get_tasks3():
 
     req = request.get_json(silent=True, force=True)
     input = req.get("queryResult").get("queryText")
-    output = {
-                "fulfillmentText"     : "Hello There. API Call Successful using {} webhook".format(input),
-                "fulfillmentMessages" : [{"text": {"text":["Hello There. API Call Successful using {} webhook".format(input)]}}]
-      
-            }
-    text   = "Hello There. API Call Successful using {} webhook".format(input)
+    
+    primarySymptoms = req.get("parameters").get("PrimarySymptom")
+    
+    text   = "You have {}".format(primarySymptoms[0])
     output = Response.makeResponse(text)
     
     return jsonify(output)
