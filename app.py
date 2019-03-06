90e457d0fb7fdd4ae9c375248f2a269e598cb0d9
prathamrg <prathamgoel95@gmail.com>
2019-03-06 18:50:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,31 +14,12 @@ from utils.helper import *
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
 
-def func1(num1, num2):
-    return num1 + num2
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks[1]})
-
-@app.route('/api2', methods=['GET'])
-def get_tasks2():
-    return jsonify(Response.makeResponse("api2 caled"))
+@app.route('/authentication', methods=['POST'])
+def authenticate():
+    req = request.get_json(silent=True, force=True)
+    return jsonify(req)
 
 @app.route('/api3', methods=['POST'])
 def get_tasks3():
