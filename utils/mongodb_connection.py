f78b143f0e3065bfaef542cb6d93a6e6997a6232
prathamrg <prathamgoel95@gmail.com>
2019-03-15 23:15:00 +0530

    Create mongodb_connection.py

---
+++ utils/mongodb_connection.py
@@ -0,0 +1,20 @@
+import pymongo
+
+class mongoDB():
+
+
+    def test(self):
+        return 1
+
+    def getData(dbname, collectionname):
+        #dbname = "guidebook"
+        #collectionname = "restaurants"
+        client = pymongo.MongoClient("mongodb+srv://pratham:mongodbatpratham95@cluster0-cjgfn.mongodb.net/test?retryWrites=true")
+        db = client[dbname]
+        collection = db[collectionname]
+
+        collection.database
+        dictionary = collection.find_one()
+        print(collection.find_one())
+        client.close()
+        return dictionary
