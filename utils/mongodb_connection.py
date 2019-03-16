ab9c905ba41759a4b61cbbc94f40d13d30da6416
prathamrg <prathamgoel95@gmail.com>
2019-03-16 20:33:17 +0530

    Update mongodb_connection.py

--- utils/mongodb_connection.py
+++ utils/mongodb_connection.py
@@ -4,14 +4,36 @@ class mongoDB():
 
 
     def getData(dbname, collectionname):
-        #dbname = "guidebook"
-        #collectionname = "restaurants"
+
         client = pymongo.MongoClient("mongodb+srv://pratham:mongodbatpratham95@cluster0-cjgfn.mongodb.net/test?retryWrites=true")
         db = client[dbname]
         collection = db[collectionname]
 
-        #collection.database
         dictionary = collection.find_one()
-        #print(collection.find_one())
         client.close()
         return dictionary
+
+    def insertData(params, dbname="patient_detail", collectionname="patient_detail"):
+
+        client = pymongo.MongoClient(
+        "mongodb+srv://pratham:mongodbatpratham95@cluster0-cjgfn.mongodb.net/test?retryWrites=true")
+        db = client[dbname]
+        collection = db[collectionname]
+
+        query = {"patient_id":params.get("patient_id")}
+
+        update = {"$set": {"patient_name":params.get("patient_name"),
+                           "patient_history_"+params.get("date"):
+                                                   {
+                                                    "symptom":params.get("symptom"),
+                                                    "symptom_severity": params.get("symptom_severity"),
+                                                    "symptom_duration": params.get("symptom_duration"),
+                                                    "sleep_pattern": params.get("sleep_pattern")
+                                                   }}}
+
+        collection.update(query,update,upsert=True)
+
+
+
+
+
