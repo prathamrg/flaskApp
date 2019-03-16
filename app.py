6406f7bbbfacb76002e5459b7bedce967a397921
prathamrg <prathamgoel95@gmail.com>
2019-03-16 20:33:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,15 +11,20 @@ from flask import request
 from utils.mongodb_connection import mongoDB
 from utils.helper import Response
 import json
+from _datetime import datetime
 
 
 
 app = Flask(__name__)
 
-@app.route('/fetch_data', methods=['GET'])
-def getData():
-    dict1 = mongoDB.getData("guidebook","restaurants")
-    return jsonify(address=dict1.get('address'),borough=dict1.get('borough'))
+@app.route('/test', methods=['POST'])
+def testMongo():
+    #dict1 = mongoDB.getData("guidebook","restaurants")
+    #return jsonify(address=dict1.get('address'),borough=dict1.get('borough'))
+    req = request.get_json(silent=True, force=True)
+    mongoDB.insertData(req)
+
+    return jsonify(req)
 
 @app.route('/authentication', methods=['POST'])
 def authenticate():
@@ -63,6 +68,18 @@ def processRequest():
                 sleepPattern    = parameters.get("SleepPattern")
                 symptomDuration = parameters.get("SymptomDuration")
         response = "You have {0} {1} since {2} and are {3}".format(symptomSeverity,primarySymptom,symptomDuration,sleepPattern)
+        params = {
+                  "patient_id": 4567,
+                  "patient_name":"Jane Doe",
+                  "date": str(datetime.utcnow()),
+                  "symptom":primarySymptom,
+                  "symptom_severity":symptomSeverity,
+                  "symptom_duration":symptomDuration,
+                  "sleep_pattern":sleepPattern
+                }
+        mongoDB.insertData(params)
+
+
 
 
 
