c56dc273ccb96ebd7efbce9b6715a35b101d9b5a
prathamrg <prathamgoel95@gmail.com>
2019-03-26 00:43:42 +0530

    Create dictionary_preprocess.py

---
+++ data/dictionary_preprocess.py
@@ -0,0 +1,33 @@
+symptom_mapping = {
+    "stomach pain":     0.6,
+    "burns":	        0.7,
+    "nose bleed":	    0.8,
+    "cuts":             0.5,
+    "dizziness":    	0.4,
+    "heat stroke":	    0.9,
+    "pulled muscle":	0.2,
+    "sprain":	        0.1,
+    "headache":	        0.3,
+    "fever":	        1
+}
+
+severity_mapping = {
+
+    "high":1,
+    "moderate":0.5,
+    "low":0.1
+}
+
+duration_mapping = {
+
+    "today":0.1,
+    "yesterday":0.5,
+    "last week":1
+
+}
+
+sleep_mapping = {
+
+    "tired":1,
+    "restless":0
+}
