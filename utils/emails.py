7976e9609f7bd96b8486f951b1de19b962ec51c3
prathamrg <prathamgoel95@gmail.com>
2019-03-26 11:06:18 +0530

    Update emails.py

--- utils/emails.py
+++ utils/emails.py
@@ -2,7 +2,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def send_email(message, patient_health):
+def send_email(payload, patient_health):
 
     # set up the SMTP server
     s = smtplib.SMTP(host='smtp.gmail.com', port=587) #port number 587 for tls using gmail smtp server
@@ -15,11 +15,15 @@ def send_email(message, patient_health):
     msg = MIMEMultipart()
     msg['From']=USERNAME
     msg['To']='prathamgoel95@gmail.com'
-    msg['Subject']="Condition {0} for Patient ID : {1}, Patient Name : {2}".format(patient_health, message.get("patient_id"), message.get("patient_name"))
+    msg['Subject']="Condition {0} for Patient ID : {1}, Patient Name : {2}".format(patient_health, payload.get("patient_id"), payload.get("patient_name"))
 
-    message = "Hello Dr. James, the patient's history is as below: \n" + str(message)
+    message = ''
+    for key in sorted(payload.keys(), reverse=True):
+        message = message + '\n' + str(key) + ': ' + str(payload[key]) + '\n'
+
+    text = "Hello Dr. James, the patient's history is as below: \n" + str(message)
     # add in the message body
-    msg.attach(MIMEText(message, 'plain'))
+    msg.attach(MIMEText(text, 'plain'))
 
     # send the message via the server set up earlier.
     s.send_message(msg)
