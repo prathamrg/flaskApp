8391ee4d1f02f5da93a72b129e2be3ab60a17b33
prathamrg <prathamgoel95@gmail.com>
2019-03-29 23:28:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Created on Thu Feb 21 19:25:43 2019
 """
 
 #!flask/bin/python
-from flask import Flask, jsonify 
+from flask import Flask, jsonify, session
 from flask import request
 from utils.mongodb_connection import mongoDB
 from utils.helper import Response
@@ -66,34 +66,16 @@ def send_email():
 
     return jsonify("Patient Health is {}. Email sent to HCP".format(patient_health))
     #return jsonify(latest)
+
+
 @app.route('/dialogflow_webhook', methods=['POST'])
 def processRequest():
 
     req = request.get_json(silent=True, force=True)
     intent = req.get("queryResult").get("intent").get("displayName")
 
-    if intent == "PrimarySymptom":
-
-        primarySymptoms = req.get("queryResult").get("parameters").get("PrimarySymptom")
-        response = "Alright. What is the severity of your {}? Low, Moderate or High?".format(primarySymptoms)
-    
-    elif intent == "SymptomSeverity":
-
-        response = "How do you feel of late? Tired or Restless?"
-
-    elif intent == "SleepPattern":
-
-        outputContexts = req.get("queryResult").get("outputContexts")
-        for outputContext in outputContexts:
-            if "primarysymptom-followup" in outputContext.get("name"):
-                parameters = outputContext.get("parameters")
-                primarySymptom = parameters.get("PrimarySymptom")
-                symptomSeverity = parameters.get("SymptomSeverity")
-                sleepPattern    = parameters.get("SleepPattern")
-
-        response = "Since when do you have {}?".format(primarySymptom)
-
-    elif intent == "SymptomDuration":
+    # Primary Symptom Follow-Up Flow:
+    if intent == "SymptomDuration":
         outputContexts = req.get("queryResult").get("outputContexts")
         for outputContext in outputContexts:
             if "primarysymptom-followup" in outputContext.get("name"):
@@ -146,6 +128,37 @@ def processRequest():
         mongoDB.closeConnection(client)
 
         response = "Your details have been saved. Please press the send button if you wish to send a notification to the nearest HCP with your details"
+
+    # Accident Follow-Up Flow:
+    elif intent == "AccidentDuration":
+        outputContexts = req.get("queryResult").get("outputContexts")
+        for outputContext in outputContexts:
+            if "accidentsymptom-followup" in outputContext.get("name"):
+                parameters = outputContext.get("parameters")
+                AccidentSymptom = parameters.get("AccidentSymptom")
+                AccidentSeverity = parameters.get("SymptomSeverity")
+                AccidentPart    = parameters.get("AccidentPart")
+                AccidentDuration = parameters.get("SymptomDuration")
+
+        params = {
+                  "patient_id": 4567,
+                  "patient_name":"Jane Doe",
+                  "date": str(datetime.strptime(datetime.now().strftime('%Y-%m-%d %H:%M'), '%Y-%m-%d %H:%M')),
+                  "symptom":AccidentSymptom,
+                  "accident_severity":AccidentSeverity,
+                  "accident_duration":AccidentDuration,
+                  "accident_part":AccidentPart
+                }
+
+        client = mongoDB.makeConnection()
+        first_aid = mongoDB.getData(client,params)
+        #mongoDB.insertData(client,params)
+        mongoDB.closeConnection(client)
+
+        response = "Thanks for providing the information. I suggest you to take the following course of preliminary action: {}. Do you want me to save your details?".format(first_aid)
+
+
+
 
     output = Response.makeResponse(response)
     
