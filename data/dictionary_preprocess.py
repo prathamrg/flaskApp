040895e2c668a7f1b347800d222742f5fe26352e
prathamrg <prathamgoel95@gmail.com>
2019-03-31 01:01:52 +0530

    Update dictionary_preprocess.py

--- data/dictionary_preprocess.py
+++ data/dictionary_preprocess.py
@@ -1,14 +1,8 @@
-symptom_mapping = {
-    "stomach pain":     0.6,
-    "burns":	        0.7,
-    "nose bleed":	    0.8,
-    "cuts":             0.5,
-    "dizziness":    	0.4,
-    "heat stroke":	    0.9,
-    "pulled muscle":	0.2,
-    "sprain":	        0.1,
-    "headache":	        0.3,
-    "fever":	        1
+# common mappings
+gender_mapping = {
+
+    "M":1,
+    "F":0
 }
 
 severity_mapping = {
@@ -26,7 +20,37 @@ duration_mapping = {
 
 }
 
+# symptom mappings
+symptom_mapping = {
+    "stomach pain":     0.6,
+    "burns":	        0.7,
+    "nose bleed":	    0.8,
+    "cuts":             0.5,
+    "dizziness":    	0.4,
+    "heat stroke":	    0.9,
+    "pulled muscle":	0.2,
+    "sprain":	        0.1,
+    "headache":	        0.3,
+    "fever":	        1
+}
+
+
 sleep_mapping = {
     "tired":1,
     "restless":0
+}
+
+# accident mappings
+accident_mapping = {
+
+    "snake bite": 0.8,
+    "burns": 0.7,
+    "nose bleed": 0.6
+}
+
+part_mapping = {
+
+    "face": 1,
+    "hand": 0.8,
+    "leg": 0.5
 }
